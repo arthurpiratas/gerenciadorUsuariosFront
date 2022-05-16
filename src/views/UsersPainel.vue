bb71b99f5c8a76f87ecfcd15ac2af5abc410ef8b
arthurpiratas <silva.arthursoares@gmail.com>
2022-05-16 20:15:44 -0300

    Incluindo modal para deleção de usuário

--- src/views/UsersPainel.vue
+++ src/views/UsersPainel.vue
@@ -17,14 +17,37 @@
             </thead>
             <tbody>
               <tr v-for="user in users" :key="user.id">
-                <td>{{user.id}}</td>
-                <td>{{user.name}}</td>
-                <td>{{user.email}}</td>
-                <td>{{user.role > 0 ? "Administrador" : "Funcionário"}}</td>
-                <td><button id="botoes" class="button is-success">Editar</button><button class="button is-danger">Deletar</button></td>
+                <td>{{ user.id }}</td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.email }}</td>
+                <td>{{ user.role > 0 ? "Administrador" : "Funcionário" }}</td>
+                <td>
+                  <button id="botoes" class="button is-success">Editar</button
+                  ><button class="button is-danger"  @click="showModals(user.id)">Deletar</button>
+                </td>
               </tr>
             </tbody>
           </table>
+        </div>
+        <div :class="{modal: true, 'is-active': showModal}">
+          <div class="modal-background"></div>
+          <div class="modal-content">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">Atenção</p>
+              </header>
+              <div class="card-content">
+                <div class="content">
+                  <p>Você deseja seguir com a exclusão do usuário?</p>
+                </div>
+              </div>
+              <footer class="card-footer">
+                <a href="#" class="card-footer-item" @click="hideModal()">NÃO</a>
+                <a href="#" class="card-footer-item">SIM</a>
+              </footer>
+            </div>
+          </div>
+          <button class="modal-close is-large" aria-label="close" @click="hideModal()"></button>
         </div>
       </div>
     </div>
@@ -37,13 +60,24 @@ export default {
   data() {
     return {
       users: [],
+      showModal: false
     };
+  },
+  methods: {
+    hideModal(){
+      this.showModal = false
+    }, 
+    showModals(id){
+      console.log(id + " ")
+      this.showModal = true
+    }
   },
   created() {
     var req = {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),
       },
+    
     };
 
     axios
@@ -61,9 +95,7 @@ export default {
 </script>
 
 <style scoped>
-
-#botoes{
-    margin-right: 5px;
-  }
-
+#botoes {
+  margin-right: 5px;
+}
 </style>
